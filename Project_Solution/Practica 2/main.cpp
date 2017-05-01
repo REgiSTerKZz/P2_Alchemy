fc47af5cad034af16f3118de9cca0bfc4bb7074c
XaviO10 <xaviercortes098@gmail.com>
2017-05-01 23:19:44 +0200

    Revert "Todo perf"
    This reverts commit b576c9485167e73b62968c49b3e18e496ba4ea9b.

--- Project_Solution/Practica 2/main.cpp
+++ Project_Solution/Practica 2/main.cpp
@@ -38,10 +38,9 @@ int main()
 	readElemets(mapa);
 	PlayerElements Alchemy;
 	Alchemy.Comandos();
-	Alchemy.printElements();
 	while (EndGame == true)
 	{
-		
+		Alchemy.printElements();
 		Alchemy.InputPlayer(mapa);
 		Alchemy.getBool(EndGame);
 	}
